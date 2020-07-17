cb983c28f68084b2b56658386073cc78851590b1
unknown <bishtjisop@gmailcom>
2020-07-17 15:14:20 +0530

    made ui with react

--- server/key.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://BishtjiGod:PumGQ0MJXFzwzJFX@instaclone.irzx6.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  JWT_SECRET: "asdgwerxcvbq12d3",
-};
-
-// PumGQ0MJXFzwzJFX
