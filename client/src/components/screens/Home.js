3997aefa3534f71a8904798d32590728f5d31fd5
unknown <bishtjisop@gmailcom>
2020-07-19 06:45:16 +0530

    added comment feature and delete post

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -30,6 +30,7 @@ const Home = () => {
     })
       .then((res) => res.json())
       .then((result) => {
+        //   console.log(result)
         const newData = data.map((item) => {
           if (item._id == result._id) {
             return result;
@@ -56,6 +57,36 @@ const Home = () => {
     })
       .then((res) => res.json())
       .then((result) => {
+        //   console.log(result)
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const makeComment = (text, postId) => {
+    fetch("/comment", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId,
+        text,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
         const newData = data.map((item) => {
           if (item._id == result._id) {
             return result;
@@ -117,6 +148,9 @@ const Home = () => {
               <img src={item.photo} />
             </div>
             <div className="card-content">
+              {/* <i className="material-icons" style={{ color: "red" }}>
+                favorite
+              </i> */}
               {item.likes.includes(state._id) ? (
                 <i
                   className="material-icons"
@@ -141,6 +175,24 @@ const Home = () => {
               <h6>{item.likes.length} likes</h6>
               <h6>{item.title}</h6>
               <p>{item.body}</p>
+              {item.comments.map((record) => {
+                return (
+                  <h6 key={record._id}>
+                    <span style={{ fontWeight: "500" }}>
+                      {record.postedBy.name}
+                    </span>{" "}
+                    {record.text}
+                  </h6>
+                );
+              })}
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  makeComment(e.target[0].value, item._id);
+                }}
+              >
+                <input type="text" placeholder="add a comment" />
+              </form>
             </div>
           </div>
         );
