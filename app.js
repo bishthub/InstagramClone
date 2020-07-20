e2744ea73f296eec1277f5c8d6606a4c4403a1c7
unknown <bishtjisop@gmailcom>
2020-07-20 09:32:58 +0530

    made everything together with build

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
-const { MONGOURI } = require("./key");
+const { MONGOURI } = require("./config/key");
 
 require("./models/user");
 require("./models/post");
@@ -23,6 +23,13 @@ mongoose.connection.on("error", (err) => {
 });
 
 mongoose.model("User");
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("server running on port ", PORT);
